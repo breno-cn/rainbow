d5adb3dd4b1577bc9527582ab19b98bc41ec4af3
Breno Caldeira Nascimento <breno.cn@hotmail.com>
2023-05-19 18:01:01 -0300

    4 senhas descobertas

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+from typing import List
+
 import subprocess
 import os
 
-def words():
+def words() -> List[List[str]]:
     with open('palavras.txt', 'r') as file:
-        return file.read().split('\n')
+        data = file.read().split('\n')
+        return list(map(lambda x: x.split(' '), data))
 
+def try_password(filename: str, word: str) -> bool:
+    command = ['openssl', 'enc', '-d', '-aes-256-cbc', '-pbkdf2', '-salt', '-in', 
+               f'./enc/{filename}', '-out', f'./dec/{filename}.dec', '-pass', f'pass:{word}']
+
+    process = subprocess.run(command, capture_output=True)
+    if process.returncode == 0:
+        print(f'Senha: {word}, arquivo: {filename}')
+        return True
+
+    return False
 
 words_list = words()
+found = False
 for file in os.listdir('./enc'):
-    for word in words_list:
-        command = ['openssl', 'enc', '-d', '-aes-256-cbc', '-pbkdf2', '-salt', '-in', f'./enc/{file}', '-out', f'./dec/{file}.dec', '-pass', f'pass:{word}']
-        process = subprocess.run(command)
-        if process.returncode == 0:
-            print(f'Senha: {word}')
+    for line in words_list:
+        for word in line:
+            if try_password(file, word):
+                found = True
+                break
+        if found:
+            found = False
             break
